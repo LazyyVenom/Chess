22651d2128a67e71104d7a11d2dd510faeadf222
Anubhav <choubey.anubhav253@gmail.com>
2024-08-15 13:32:04 +0530

    Added word

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ DARK_BLUE = (0, 105, 153)
 LIGHT_BLUE = (230, 247, 255)
 HIGHLIGHT_COLOR1 = (255, 179, 179)
 HIGHLIGHT_COLOR2 = (255, 102, 102)
-PIECE_COLOR = (102, 153, 255)
+PIECE_PLACEMENT_COLOR = (102, 153, 255)
 
 b_king_available = True
 b_l_rook_available = True
@@ -439,7 +439,7 @@ def draw_board(window, highlights,selected=None):
                 color = colors[(row + col) % 2]
             
             if selected and (row,col) == selected:
-                color = PIECE_COLOR
+                color = PIECE_PLACEMENT_COLOR
 
             pygame.draw.rect(window, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
